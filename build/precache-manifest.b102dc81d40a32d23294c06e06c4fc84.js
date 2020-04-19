40820bd3991ecc87ef81b43aa7352da5e01412f4
Laura <lbk.laura@gmail.com>
2020-04-19 15:59:43 +0200

    Builf

---
+++ build/precache-manifest.b102dc81d40a32d23294c06e06c4fc84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b59b85b1ac5b6de1068eab4eef10ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63b61a77f082cf5d7f6d",
+    "url": "/static/css/main.df2a8486.chunk.css"
+  },
+  {
+    "revision": "5dadbe337f5fd61b5b49",
+    "url": "/static/js/2.e49cd94a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e49cd94a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63b61a77f082cf5d7f6d",
+    "url": "/static/js/main.c44c2282.chunk.js"
+  },
+  {
+    "revision": "0296cfa5bed386b57cf9",
+    "url": "/static/js/runtime-main.9a061314.js"
+  }
+]);
